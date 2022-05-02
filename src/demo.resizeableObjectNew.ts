2dd32c5f68a401672c67be961209b90adaf0577f
kate <kate@dariox.club>
2022-05-02 13:48:04 +0800

    Add image as a container.

--- src/demo.resizeableObjectNew.ts
+++ src/demo.resizeableObjectNew.ts
@@ -20,6 +20,7 @@ export default class DemoResizeableObject extends DemoBase
 
     public DemoInstance: ResizeableObject
     public Target: PIXI.IDisplayObjectExtended
+    public TargetContainer: PIXI.Container
 
     public async fetchTestImage(index: number, scale: PIXI.Point = new PIXI.Point(1, 1)) : Promise<PIXI.Sprite>
     {
@@ -32,7 +33,15 @@ export default class DemoResizeableObject extends DemoBase
     }
     public async Initalize() : Promise<void>
     {
+        this.TargetContainer = new PIXI.Container()
         this.Target = await this.fetchTestImage(0, new PIXI.Point(0.1, 0.1))
-        this.DemoInstance.setTarget(this.Target)
+        let targetBounds = this.Target.getBounds()
+        this.TargetContainer.x = 100
+        this.TargetContainer.y = 100
+        this.TargetContainer.width = targetBounds.width
+        this.TargetContainer.height = targetBounds.height
+        this.TargetContainer.addChild(this.Target)
+        this.DemoInstance.setTarget(this.TargetContainer)
+        this.Container.addChild(this.TargetContainer)
     }
 }
