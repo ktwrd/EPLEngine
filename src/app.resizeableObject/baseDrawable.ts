b3f6f75aaff8ded02a779d10f8acc59c4eaef08f
kate <kate@dariox.club>
2022-05-02 13:47:26 +0800

    Create IBaseDrawable

---
+++ src/app.resizeableObject/baseDrawable.ts
@@ -0,0 +1,10 @@
+import * as PIXI from 'pixi.js'
+
+import {IResizeableObject} from './index' 
+
+export default interface IBaseDrawable
+{
+    draw(options: any, bounds: PIXI.Rectangle) : void
+    Graphics: PIXI.Graphics
+    ResizeableObject: IResizeableObject
+}
