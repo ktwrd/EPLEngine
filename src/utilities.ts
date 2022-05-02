ba9ffd294df80c734db5a613eecb6ffdfcf5fe59
kate <kate@dariox.club>
2022-05-02 16:52:56 +0800

    Add snap to grid methods

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,7 +1,20 @@
+import * as PIXI from 'pixi.js'
+
 export function enumKeysAsString<TEnum>(theEnum: TEnum): keyof TEnum {
     // eliminate numeric keys
     const keys = Object.keys(theEnum).filter(x => 
     (+x)+"" !== x) as (keyof TEnum)[];
     // return some random key
     return keys[Math.floor(Math.random()*keys.length)]; 
+}
+export function numberSnapToGrid(point: number, gridSize: number): number
+{
+    return Math.round(point / gridSize) * gridSize
+}
+export function pointSnapToGrid(point: PIXI.Point, gridSize: number): PIXI.Point
+{
+    return new PIXI.Point(
+        numberSnapToGrid(point.x, gridSize),
+        numberSnapToGrid(point.y, gridSize)
+    )
 }
