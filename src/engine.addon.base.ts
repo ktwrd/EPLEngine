f810fca75ad1333f0ee1e87923bfb679f2b84669
kate <kate@dariox.club>
2022-05-03 11:24:29 +0800

    Create base Engine Addon

---
+++ src/engine.addon.base.ts
@@ -0,0 +1,29 @@
+import * as PIXI from 'pixi.js'
+
+import Engine, {IEngineAddon} from './engine'
+
+export default class BaseEngineAddon implements IEngineAddon
+{
+    public constructor(engine: Engine)
+    {
+        this.Engine = engine
+    }
+
+    public Engine: Engine
+    public Container: PIXI.Container = new PIXI.Container()
+
+    public enabled: boolean = false
+    public enable() : void
+    {
+        if (this.enabled) return
+        this.enabled = true
+        this.Engine.Application.stage.addChild(this.Container)
+    }
+    public disable() : void
+    {
+        if (!this.enabled) return
+        this.enabled = false
+
+        this.Engine.Application.stage.removeChild(this.Container)
+    }
+}
