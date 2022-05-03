014e8b034544425cce45b3602761ac15dc8243f6
kate <kate@dariox.club>
2022-05-03 15:55:27 +0800

    Use generated textures instead of hard-coded images

--- src/demo.resizeableObject.ts
+++ src/demo.resizeableObject.ts
@@ -3,10 +3,9 @@ import * as PIXI from 'pixi.js'
 import Engine from './engine'
 import DemoBase from './demobase'
 
-import * as _imgData from './image'
 import ResizeableObject from './app.resizeableObject/index'
 
-const ImageData: string[] = _imgData.Data
+import {TextureGenerators} from './demo.textureGenerators'
 
 export default class DemoResizeableObject extends DemoBase
 {
@@ -22,27 +21,34 @@ export default class DemoResizeableObject extends DemoBase
     public Target: PIXI.IDisplayObjectExtended
     public TargetContainer: PIXI.Container
 
-    public async fetchTestImage(index: number, scale: PIXI.Point = new PIXI.Point(1, 1)) : Promise<PIXI.Sprite>
+    public async fetchGeneratorImage(index: number, scale: PIXI.Point = new PIXI.Point(1, 1)) : Promise<PIXI.Sprite>
     {
-        let teture = await PIXI.Texture.fromURL(ImageData[index])
-        let sprite = new PIXI.Sprite(teture)
+        let item = TextureGenerators[index]()
+        item.setTransform()
+        let bounds = item.getBounds()
+        let renderTexture = PIXI.RenderTexture.create({ width: bounds.width + bounds.x, height: bounds.height + bounds.y });
+        this.Engine.Application.renderer.render(item, {renderTexture})
+
+        let sprite = new PIXI.Sprite(renderTexture)
         sprite.scale.set(scale.x, scale.y)
         sprite.x = 0
         sprite.y = 0
         return sprite
     }
+
     public async Initalize() : Promise<void>
     {
         this.TargetContainer = new PIXI.Container()
-        this.Target = await this.fetchTestImage(0, new PIXI.Point(1, 1))
+        this.Target = await this.fetchGeneratorImage(0, new PIXI.Point(10, 10))
         let targetBounds = this.Target.getBounds()
         this.TargetContainer.x = 100
         this.TargetContainer.y = 100
         this.TargetContainer.width = targetBounds.width
         this.TargetContainer.height = targetBounds.height
         this.TargetContainer.addChild(this.Target)
-        this.TargetContainer.width = 256
-        this.TargetContainer.height = 108
+        let spriteBounds = this.Target.getBounds()
+        this.TargetContainer.width = spriteBounds.width / 10
+        this.TargetContainer.height = spriteBounds.height / 10
         this.DemoInstance.setTarget(this.TargetContainer)
         this.Container.addChild(this.DemoInstance.Container)
     }
