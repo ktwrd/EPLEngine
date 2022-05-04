512100fd6354945c7279b2ca5d5158ca7b541c0d
kate <kate@dariox.club>
2022-05-04 16:43:25 +0800

    Make method fetchGeneratorImage static

--- src/demo.resizeableObject.ts
+++ src/demo.resizeableObject.ts
@@ -21,13 +21,13 @@ export class DemoResizeableObject extends DemoBase
     public Target: PIXI.IDisplayObjectExtended
     public TargetContainer: PIXI.Container
 
-    public async fetchGeneratorImage (index: number, scale: PIXI.Point = new PIXI.Point(1, 1)): Promise<PIXI.Sprite>
+    public static async fetchGeneratorImage (engine: Engine, index: number, scale: PIXI.Point = new PIXI.Point(1, 1)): Promise<PIXI.Sprite>
     {
         let item = TextureGenerators[index]()
         item.setTransform()
         let bounds = item.getBounds()
         let renderTexture = PIXI.RenderTexture.create({ width: bounds.width + bounds.x, height: bounds.height + bounds.y })
-        this.Engine.Application.renderer.render(item, { renderTexture })
+        engine.Application.renderer.render(item, { renderTexture })
 
         let sprite = new PIXI.Sprite(renderTexture)
         sprite.scale.set(scale.x, scale.y)
@@ -39,7 +39,7 @@ export class DemoResizeableObject extends DemoBase
     public async Initalize (): Promise<void>
     {
         this.TargetContainer = new PIXI.Container()
-        this.Target = await this.fetchGeneratorImage(0, new PIXI.Point(10, 10))
+        this.Target = await DemoResizeableObject.fetchGeneratorImage(this.Engine, 0, new PIXI.Point(10, 10))
         let targetBounds = this.Target.getBounds()
         this.TargetContainer.x = 100
         this.TargetContainer.y = 100
