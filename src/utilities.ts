fb6913c0f576162ca2e138fe4fce03394965f428
kate <kate@dariox.club>
2022-05-04 16:44:25 +0800

    Make Utilities a Static Class

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,21 +1,27 @@
 import * as PIXI from 'pixi.js'
+import * as EPLEngine from './index'
 
-export function enumKeysAsString<TEnum> (theEnum: TEnum): keyof TEnum
+import { TextureGenerators } from './demo.textureGenerators'
+export class Utilities
 {
-    // eliminate numeric keys
-    const keys = Object.keys(theEnum).filter(x =>
-        (+x) + "" !== x) as (keyof TEnum)[]
-    // return some random key
-    return keys[Math.floor(Math.random() * keys.length)]
+    public static enumKeysAsString<TEnum> (theEnum: TEnum): keyof TEnum
+    {
+        // eliminate numeric keys
+        const keys = Object.keys(theEnum).filter(x =>
+            (+x) + "" !== x) as (keyof TEnum)[]
+        // return some random key
+        return keys[Math.floor(Math.random() * keys.length)]
+    }
+    public static numberSnapToGrid (point: number, gridSize: number): number
+    {
+        return Math.round(point / gridSize) * gridSize
+    }
+    public static pointSnapToGrid (point: PIXI.Point, gridSize: number): PIXI.Point
+    {
+        return new PIXI.Point(
+            Utilities.numberSnapToGrid(point.x, gridSize),
+            Utilities.numberSnapToGrid(point.y, gridSize)
+        )
+    }
 }
-export function numberSnapToGrid (point: number, gridSize: number): number
-{
-    return Math.round(point / gridSize) * gridSize
-}
-export function pointSnapToGrid (point: PIXI.Point, gridSize: number): PIXI.Point
-{
-    return new PIXI.Point(
-        numberSnapToGrid(point.x, gridSize),
-        numberSnapToGrid(point.y, gridSize)
-    )
-}
+export default Utilities
