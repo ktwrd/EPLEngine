f808c0884d11d4262a12c08d76b3bcd22466292b
kate <kate@dariox.club>
2022-05-10 11:44:01 +0800

    Update src/_PACKAGE.ts

--- src/_PACKAGE.ts
+++ src/_PACKAGE.ts
@@ -8,8 +8,8 @@ export interface IPackageInfo
 }
 export const PackageInfo: IPackageInfo = {
     name: "eplengine",
-    version: "1.0.5",
+    version: "1.0.7",
     author: "Kate Ward <kate@dariox.club>",
     license: "AGPL-3.0",
-    _buildtimestamp: 1652147061259
+    _buildtimestamp: 1652154224219
 }
